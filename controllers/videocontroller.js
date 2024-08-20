2d62dfcd8203838d0c5d768afb3c634a2ee859c9
deepanshu verma <deepanshuverma186@gmail.com>
2024-08-20 13:04:34 -0700

    video update controller

--- controllers/videocontroller.js
+++ controllers/videocontroller.js
@@ -16,7 +16,6 @@ export const uploadVideo = async (req, res) => {
 
     const videoFileUri = getDataUri(videoFile);
   
-
     const uploadVideo = await cloudinary.v2.uploader.upload(videoFileUri.content, {
       resource_type: 'video'
     });
@@ -51,3 +50,81 @@ export const uploadVideo = async (req, res) => {
     });
   }
 };
+
+
+
+export const getVideoById =async(req,res)=>{
+    try {
+       const { id } = req.params
+       const singleVideo = await videoModel.findById(id)
+       res.status(200).json(singleVideo)       
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Internal Server Error"
+          });
+    }
+}
+
+
+export const getVideos=async(req,res)=>{
+    try {
+        const feed = await videoModel.find({})
+        res.status(200).json(feed)       
+     } catch (error) {
+         console.log(error)
+         res.status(500).json({
+             message: "Internal Server Error"
+           });
+     }
+}
+export const getVideosByUser=async(req,res)=>{
+    try {
+        const { id } = req.params 
+        const userVideos = await videoModel.find({uploadBy:id})
+        res.status(200).json(userVideos)       
+     } catch (error) {
+         console.log(error)
+         res.status(500).json({
+             message: "Internal Server Error"
+           });
+     }
+}
+
+
+
+
+export const updateVideo = async (req, res) => {
+  try {
+    const userId = req.user._id; 
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const video = await videoModel.findOne({ _id: id, uploadBy: userId });
+
+    if (!video) {
+      return res.status(404).json({
+        message: 'Video not found or Invalid User',
+      });
+    }
+
+    // Update the video details
+    video.title = title || video.title;
+    video.description = description || video.description;
+    await video.save();
+
+    res.status(200).json({
+      message: 'Video Updated Successfully',
+      video,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+
+
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { uploadVideo } from '../controllers/videocontroller.js'
+import { getVideoById, getVideos, getVideosByUser, updateVideo, uploadVideo } from '../controllers/videocontroller.js'
 import multiUpload from '../middlewares/multipart.js'
 const router = express()
 
 router.post('/upload',authMiddleware,multiUpload,uploadVideo)
+
+router.get('/single/:id',getVideoById)
+
+router.get('/feed',getVideos)
+
+router.put('/update/:id',authMiddleware, updateVideo)
+
+router.get('/user/:id', getVideosByUser)
 
 export default router
