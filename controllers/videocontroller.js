b710759b9824ff8803a6e5e8ccccea8857a72894
deepanshu verma <deepanshuverma186@gmail.com>
2024-08-20 23:22:04 -0700

    add delete video

--- controllers/videocontroller.js
+++ controllers/videocontroller.js
@@ -126,5 +126,26 @@ export const updateVideo = async (req, res) => {
 };
 
 
-
+export const deleteVideo=async(req,res)=>{
+  try {
+    const userId = req.user._id; 
+    const { id } = req.params;
+    const video = await videoModel.findOne({ _id: id, uploadBy: userId });
+    if (!video) {
+      return res.status(404).json({
+        message: 'Video not found or Invalid User',
+      });
+    }
+    await video.deleteOne()
+    res.status(200).json({
+      message: 'Video Deleted Successfully'
+    });
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
 
